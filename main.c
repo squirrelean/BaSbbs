fa2141aa9188d5986fb17b183f95df9d23ef59a5
squirrelean <danastasio02@icloud.com>
2026-03-02 18:50:30 -0500

    call single threaded server

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "server.h"
+
 #include "read_config.h"
 int main(int argc, char *argv[])
 {
     if (argc > 2) {
         printf("Too many arguments\n");
-        exit(EXIT_SUCCESS);
+        exit(EXIT_FAILURE);
     }
 
     ServerConfig config = {.thmax = 25, .thincr = 5, .bbport = 9000, .fdebug = false, .bbfile = NULL};
@@ -21,11 +23,13 @@ int main(int argc, char *argv[])
     read_config_file(&config, config_path);
     if (!config.bbfile) {
         printf("BBFILE required\n");
-        exit(EXIT_SUCCESS);
+        exit(EXIT_FAILURE);
     }
 
     printf("thmax: %d, thincr: %d, bbport: %d, fdebug: %d, bbfile: %s", config.thmax, config.thincr,
            config.bbport, config.fdebug, config.bbfile);
+
+    initialize_server(&config);
 
     free(config.bbfile);
 }
