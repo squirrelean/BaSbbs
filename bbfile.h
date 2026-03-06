245d505e142a38b59a9cdb2118c079ad696c6669
squirrelean <danastasio02@icloud.com>
2026-03-06 18:26:51 -0500

    refactor bb_init to use global config struct

--- bbfile.h
+++ bbfile.h
@@ -1,7 +1,7 @@
 #ifndef BBFILE_H
 #define BBFILE_H
 
-int bb_init(const char *bbfile_path);
+int bb_init();
 int bb_write(const char *poster, const char *message);
 int bb_read(const long message_number, char **message);
 int bb_replace(const char *username, const long message_number, const char *new_message);
