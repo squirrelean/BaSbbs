14f7009ad3d9d040fe8e40a407f29c69c1d5bf0e
squirrelean <danastasio02@icloud.com>
2026-03-19 22:10:18 -0400

    implement replication var support

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 #include "read_config.h"
 
 ServerConfig global_config;
+ReplicationConfig global_rconfig;
+
+void print_config();
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +21,7 @@ int main(int argc, char *argv[])
     }
 
     global_config = (ServerConfig){.thmax = 25, .thincr = 5, .bbport = 9000, .fdebug = false, .bbfile = NULL};
+    global_rconfig = (ReplicationConfig){.fground = 0, .pdebug = 0, .rport = 9001, .peer = NULL};
 
     char *config_path;
     if (argc == 2)
@@ -25,18 +29,30 @@ int main(int argc, char *argv[])
     else
         config_path = "bbserv.conf";
 
-    read_config_file(&global_config, config_path);
+    read_config_file(&global_config, &global_rconfig, config_path);
     if (!global_config.bbfile) {
         printf("BBFILE required\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("thmax: %d, thincr: %d, bbport: %d, fdebug: %d, bbfile: %s\n", global_config.thmax,
-           global_config.thincr, global_config.bbport, global_config.fdebug, global_config.bbfile);
+    print_config();
 
     bb_init();
 
     initialize_server();
 
     free(global_config.bbfile);
+    free_peers(global_rconfig.peer, global_rconfig.peer_count);
+}
+
+void print_config()
+{
+    printf("thmax: %d, thincr: %d, bbport: %d, fdebug: %d, bbfile: %s\n", global_config.thmax,
+           global_config.thincr, global_config.bbport, global_config.fdebug, global_config.bbfile);
+
+    printf("forground: %d, pdebug: %d, rport: %d\n", global_rconfig.fground, global_rconfig.pdebug,
+           global_rconfig.rport);
+
+    for (int i = 0; i < global_rconfig.peer_count; i++)
+        printf("peer #%d. Host: %s, Port: %d\n", i, global_rconfig.peer[i].host, global_rconfig.peer[i].port);
 }
