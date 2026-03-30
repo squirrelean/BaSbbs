f5b448830a229e7dff211886803c8aff28fc99a3
squirrelean <danastasio02@icloud.com>
2026-03-30 10:34:47 -0400

    add global reconfiguration flags

--- globals.h
+++ globals.h
@@ -1,8 +1,11 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 #include "read_config.h"
+#include <signal.h>
 
 extern ServerConfig global_config;
 extern ReplicationConfig global_rconfig;
+extern volatile sig_atomic_t global_restart_server;
+extern volatile sig_atomic_t global_terminate_server;
 
 #endif
