667b489c6088ff2f374006f01cd6337f790f701a
squirrelean <danastasio02@icloud.com>
2026-04-09 20:19:02 -0400

    header for additional utils

--- tcp_utils.h
+++ tcp_utils.h
@@ -4,5 +4,8 @@
 
 int create_listen_socket(int port, int backlog);
 int read_line(const int fd, char *buffer, const size_t maxlen);
+int get_connection_info(const char *rhost, const int rport, struct sockaddr_in *sin);
+int connect_to_server(struct sockaddr_in *sin, int *sd);
+char *read_from_server(int sd, int timeout);
 
 #endif
