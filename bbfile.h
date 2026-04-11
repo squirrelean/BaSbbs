339222f05ad43c3bf1a8da1df2071773d3bba564
squirrelean <danastasio02@icloud.com>
2026-04-11 13:59:55 -0400

    updated header

--- bbfile.h
+++ bbfile.h
@@ -1,10 +1,18 @@
 #ifndef BBFILE_H
 #define BBFILE_H
 
+typedef struct {
+    long file_offset;
+    char *backup;
+    long previous_id;
+    int status;
+} BbMeta;
+
 int bb_init();
-int bb_write(const char *poster, const char *message);
+BbMeta bb_write(const char *poster, const char *message);
 int bb_read(const long message_number, char **message);
-int bb_replace(const char *username, const long message_number, const char *new_message);
-long get_bbfile_offset();
+BbMeta bb_replace(const char *username, const long message_number, const char *new_message);
+void bb_rollback(BbMeta meta);
+void delete_backup(BbMeta meta);
 
 #endif
